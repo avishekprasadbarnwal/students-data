63cdf985083af0809cc3639c9e32470fe86e6e2c
avishekprasadbarnwal <49061068+avishekprasadbarnwal@users.noreply.github.com>
2019-04-02 12:06:24 +0530

    Add files via upload

---
+++ data.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	int a=10,c;
+	printf("the age of the individual is %d",&a);
+	for(i=1;i<=5;i++)
+	{	printf("enter the marks of subject %d",i);
+		scanf("%d",&c);
+	}
+}
